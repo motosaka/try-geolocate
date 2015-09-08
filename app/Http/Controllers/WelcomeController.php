a1a24f29499c044dbc24b4bb6884b97bcc22ff0d
motosaka <motosaka@gmail.com>
2015-09-08 11:50:59 +0900

    (try) typo: input

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -30,7 +30,7 @@ class WelcomeController extends Controller {
 	 */
 	public function index()
 	{
-		$input = \Request::all();
+		$inputs = \Request::all();
 		return view('welcome')->with('inputs', $inputs);
 	}
 
