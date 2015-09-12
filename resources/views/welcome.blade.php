1c9e4dd6634c3987439ac75db09075f018bf8cc9
motosaka <motosaka@gmail.com>
2015-09-12 23:39:30 +0900

    (try)Mod debug form.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,12 @@
 
 <br/><br/><br/><br/>
 <form>
-<input id="to_inputbox" type="text" name="timeout" value="{{ $inputs['timeout'] or '' }}">
-<input id="init_inputbox" type="text" name="init_mode" value="{{ $inputs['init_mode'] or '' }}">
-<input type="button" onClick="$.removeCookie('geolocate-confirm');" value="(テスト用) クッキー削除">
+<dl>
+<dt>タイムアウト値</dt><dd><input id="to_inputbox" type="text" name="timeout" value="{{ $inputs['timeout'] or '' }}"></dd>
+<dt>動作モード(confirm,noconfirm,空)</dt><dd><input id="init_inputbox" type="text" name="init_mode" value="{{ $inputs['init_mode'] or '' }}"></dd>
+<dt>再読み込み</dt><dd><input type="submit" value="リロード"></dd>
+<dt>クッキー削除</dt><dd><input type="button" onClick="$.removeCookie('geolocate-confirm');" value="(テスト用) クッキー削除"></dd>
+</dl>
 </form>
 @endsection
 @section('geo_script')
