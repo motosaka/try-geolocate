fcd609939d2ab11c6d75626b971145da89daadc2
motosaka <motosaka@gmail.com>
2015-09-12 23:15:11 +0900

    (try)5 js expanded, and getScript into jQ-func.

--- public/js/call-js1.js
+++ public/js/call-js1.js
@@ -1,9 +1,7 @@
-// 最低限jQueryは事前に呼んでること
-$.getScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js");
-$.getScript("//maps.google.com/maps/api/js?sensor=true");
-$.getScript("/js/sweetalert.min.js");
-$.getScript("/js/gmaps.js");
 $(function($){
+    // 最低限jQueryは事前に呼んでること
+    $.getScript("//maps.google.com/maps/api/js?sensor=true");
+    $.getScript("/js/gmaps.js");
 
     var queries = (function(){
 	var s = location.search.replace("?", "");
