d8d5fab73f6c674082a7f49d3f26b2c43c9e6da9
motosaka <motosaka@gmail.com>
2015-09-12 23:10:49 +0900

    (try)2 js expanded.

--- public/js/call-js1.js
+++ public/js/call-js1.js
@@ -1,5 +1,4 @@
 // 最低限jQueryは事前に呼んでること
-$.getScript("//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js");
 $.getScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js");
 $.getScript("//maps.google.com/maps/api/js?sensor=true");
 $.getScript("/js/sweetalert.min.js");
